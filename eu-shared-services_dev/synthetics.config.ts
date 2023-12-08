25bad34841a64df0a105372699105105a3aa4a1b
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2023-12-08 20:57:07 +0100

    Added AWS Synthetic Monitoring

---
+++ eu-shared-services_dev/synthetics.config.ts
@@ -0,0 +1,35 @@
+import type { SyntheticsConfig } from '@elastic/synthetics';
+
+export default env => {
+  const config: SyntheticsConfig = {
+    params: {
+      url: 'https://elastic.github.io/synthetics-demo/',
+    },
+    playwrightOptions: {
+      ignoreHTTPSErrors: false,
+    },
+    /**
+     * Configure global monitor settings
+     */
+    monitor: {
+      schedule: 1,
+      locations: [],
+      privateLocations: ['eu-shared-services_dev'],
+    },
+    /**
+     * Project monitors settings
+     */
+    project: {
+      id: 'eu-shared-services_dev',
+      url: 'https://kibana-europe.pvhcorp.com',
+      space: 'developer-experience',
+    },
+  };
+  if (env !== 'development') {
+    /**
+     * Override configuration specific to environment
+     * Ex: config.params.url = ""
+     */
+  }
+  return config;
+};
