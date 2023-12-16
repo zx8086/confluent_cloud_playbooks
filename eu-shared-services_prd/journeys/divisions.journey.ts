c3fe3ca81e654654cb9b3a075b29ff29804e1237
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2023-12-16 13:11:19 +0100

    Adding Playwright Testing

--- eu-shared-services_prd/journeys/divisions.journey.ts
+++ eu-shared-services_prd/journeys/divisions.journey.ts
@@ -8,66 +8,108 @@ journey('DS - API Journey | Divisions - prd', async ({ context }: { context: any
   });
 
   step('GET Brands API', async () => {
-    const response = await context.request.get('https://divisions.prd.shared-services.eu.pvh.cloud/v1/brands');
+    const brandResponse = await context.request.get('https://divisions.prd.shared-services.eu.pvh.cloud/v1/brands');
+    const brandBody = await brandResponse.json();
+    console.log(JSON.stringify(brandBody));
 
-    // Verify the response
-    const body = await response.json();
-    console.log(JSON.stringify(body));
-
-    // Verify the status code
-    verifyStatusCode(response);
-
-    // Parse the JSON response
-    const responseBody = await response.json();
-
-    // Verify the response structure and content
-    verifyResponseStructure(responseBody);
-
-    // Verify specific details in the JSON response
-    verifyCompanyDetails(responseBody);
+    verifyResponse(brandResponse, 200);
+    verifyResponseStructure(brandBody);
+    verifyCompanyDetails(brandBody);
   });
 
   step('GET Divisions for a Brand API', async () => {
-    const response = await context.request.get('https://divisions.prd.shared-services.eu.pvh.cloud/v1/divisions/TH/division/01');
+    const divisionResponse = await context.request.get('https://divisions.prd.shared-services.eu.pvh.cloud/v1/divisions/TH/division/01');
+    const divisionBody = await divisionResponse.json();
+    console.log(JSON.stringify(divisionBody));
 
-    // Verify the response
-    const body = await response.json();
-    console.log(JSON.stringify(body));
-
-    // Verify the status code
-    expect(response.status()).toBe(200);
-
-    // Parse the JSON response
-    const responseBody = await response.json();
-
-    // Verify the response structure
-    verifyResponseStructureDivisionBrand(responseBody);
-
-    // Step 4: Validate date fields as dates
-    validateDateFields(responseBody);
+    verifyResponse(divisionResponse, 200);
+    verifyResponseStructureDivisionBrand(divisionBody);
+    validateDateFields(divisionBody);
   });
 
   step('GET a season for a brand', async () => {
-    const response = await context.request.get('https://divisions.prd.shared-services.eu.pvh.cloud/v1/divisions/TH/division/01/seasons/C41');
+    const seasonResponse = await context.request.get('https://divisions.prd.shared-services.eu.pvh.cloud/v1/divisions/TH/division/01/seasons/C41');
+    const seasonBody = await seasonResponse.json();
+    console.log(JSON.stringify(seasonBody));
 
-    // Verify the response
-    const body = await response.json();
-    console.log(JSON.stringify(body));
-
-    // Verify the status code
-    expect(response.status()).toBe(200);
-
-    // Parse the JSON response
-    const responseBody = await response.json();
-
-    // Verify the response structure
-    verifyResponseStructureSeasonBrand(responseBody);
-
-    // Validate date values in orderFulfillmentTypes
-    validateDateValuesSeasonBrand(responseBody);
+    verifyResponse(seasonResponse, 200);
+    verifyResponseStructureSeasonBrand(seasonBody);
+    validateDateValuesSeasonBrand(seasonBody);
   });
 
-  // Get Seasons for a Brand
+  function verifyResponse(response, expectedStatusCode) {
+    expect(response.status()).toBe(expectedStatusCode);
+  }
+
+  function verifyResponseStructure(responseBody) {
+    expect(responseBody).toHaveProperty('items');
+    expect(Array.isArray(responseBody.items)).toBe(true);
+    expect(responseBody).toHaveProperty('count');
+    expect(typeof responseBody.count).toBe('number');
+    expect(responseBody).toHaveProperty('hasNextPage');
+    expect(typeof responseBody.hasNextPage).toBe('boolean');
+    expect(responseBody).toHaveProperty('skipToken');
+  }
+
+  function verifyCompanyDetails(responseBody) {
+    const items = responseBody.items;
+    expect(items).toHaveLength(7);
+
+    // Verify details for each company code
+    const companyDetails = [
+      {
+        companyCode: 'CKEU',
+        name: 'Calvin Klein',
+        divisionCodes: ['61', '62', '63', '65', '67', '68', '69', '70', '71', '72', '77'],
+        salesOrganizationCodes: ['CK07', 'INCK'],
+      },
+      {
+        companyCode: 'THEU',
+        name: 'Tommy Hilfiger',
+        divisionCodes: ['01','02','03','04','05','07','08','09','10','11','18'],
+        salesOrganizationCodes: ['THE1', 'INLC'],
+      },
+      {
+        companyCode: 'IZEU',
+        name: 'IZOD',
+        divisionCodes: ['92'],
+        salesOrganizationCodes: ['PBE1'],
+      },
+      {
+        companyCode: 'MK',
+        name: 'Michael Kors',
+        divisionCodes: ['91'],
+        salesOrganizationCodes: ['PBE1'],
+      },
+    ];
+
+    for (const detail of companyDetails) {
+      const company = items.find((item) => item.companyCode === detail.companyCode);
+      expect(company).toBeDefined();
+      expect(company.name).toBe(detail.name);
+      expect(company.divisionCodes).toEqual(expect.arrayContaining(detail.divisionCodes));
+      expect(company.salesOrganizationCodes).toEqual(expect.arrayContaining(detail.salesOrganizationCodes));
+    }
+  }
+
+  function verifyResponseStructureDivisionBrand(responseBody) {
+    expect(responseBody).toHaveProperty('name');
+    expect(responseBody).toHaveProperty('code');
+    expect(responseBody).toHaveProperty('weCTimestamp');
+    expect(responseBody).toHaveProperty('modifiedOn');
+    expect(responseBody).toHaveProperty('seasons');
+    expect(Array.isArray(responseBody.seasons)).toBe(true);
+  }
+
+  function validateDateFields(responseBody) {
+    const dateFields = ['weCTimestamp', 'modifiedOn'];
+    for (const field of dateFields) {
+      const fieldValue = new Date(responseBody[field]);
+      expect(fieldValue).toBeInstanceOf(Date);
+      expect(!isNaN(fieldValue)).toBe(true);
+    }
+  }
+
   function verifyResponseStructureSeasonBrand(responseBody) {
     expect(responseBody).toHaveProperty('name');
     expect(responseBody).toHaveProperty('code');
@@ -85,117 +127,11 @@ journey('DS - API Journey | Divisions - prd', async ({ context }: { context: any
     expect(season).toHaveProperty('orderFulfillmentTypes');
     expect(Array.isArray(season.orderFulfillmentTypes)).toBe(true);
 
-    const orderFulfillmentTypes = season.orderFulfillmentTypes;
-    for (const type of orderFulfillmentTypes) {
-      expect(type).toHaveProperty('code');
-      expect(type).toHaveProperty('deliveryDates');
-      expect(Array.isArray(type.deliveryDates)).toBe(true);
-
-      // Validate date values in deliveryDates
-      for (const date of type.deliveryDates) {
-        const parsedDate = new Date(date);
-        expect(parsedDate).toBeInstanceOf(Date);
-        expect(!isNaN(parsedDate)).toBe(true);
-      }
+    const dateFields = season.orderFulfillmentTypes.map((type) => type.deliveryDates).flat();
+    for (const date of dateFields) {
+      const parsedDate = new Date(date);
+      expect(parsedDate).toBeInstanceOf(Date);
+      expect(!isNaN(parsedDate)).toBe(true);
     }
   }
-
-// GET Brands API
-  function verifyStatusCode(response) {
-    expect(response.status()).toBe(200);
-  }
-
-  function verifyResponseStructure(responseBody) {
-    expect(responseBody).toHaveProperty('items');
-    expect(Array.isArray(responseBody.items)).toBe(true);
-    expect(responseBody).toHaveProperty('count');
-    expect(typeof responseBody.count).toBe('number');
-    expect(responseBody).toHaveProperty('hasNextPage');
-    expect(typeof responseBody.hasNextPage).toBe('boolean');
-    expect(responseBody).toHaveProperty('skipToken');
-  }
-
-  function verifyCompanyDetails(responseBody) {
-    const items = responseBody.items;
-    expect(items).toHaveLength(7);
-
-    // Example: Verify details for a specific company (e.g., Calvin Klein)
-    const calvinKlein = items.find((item) => item.companyCode === 'CKEU');
-    expect(calvinKlein).toBeDefined();
-    expect(calvinKlein.name).toBe('Calvin Klein');
-    expect(calvinKlein.divisionCodes).toContain('61');
-    expect(calvinKlein.divisionCodes).toContain('62');
-    expect(calvinKlein.divisionCodes).toContain('63');
-    expect(calvinKlein.divisionCodes).toContain('65');
-    expect(calvinKlein.divisionCodes).toContain('67');
-    expect(calvinKlein.divisionCodes).toContain('68');
-    expect(calvinKlein.divisionCodes).toContain('69');
-    expect(calvinKlein.divisionCodes).toContain('70');
-    expect(calvinKlein.divisionCodes).toContain('71');
-    expect(calvinKlein.divisionCodes).toContain('72');
-    expect(calvinKlein.divisionCodes).toContain('77');
-    expect(calvinKlein.salesOrganizationCodes).toContain('CK07');
-    expect(calvinKlein.salesOrganizationCodes).toContain('INCK');
-
-    // Verify details for IZOD
-    const izod = items.find((item) => item.companyCode === 'IZEU');
-    expect(izod).toBeDefined();
-    expect(izod.name).toBe('IZOD');
-    expect(izod.divisionCodes).toContain('92');
-    expect(izod.salesOrganizationCodes).toContain('PBE1');
-
-    // Verify details for Michael Kors
-    const michaelKors = items.find((item) => item.companyCode === 'MK');
-    expect(michaelKors).toBeDefined();
-    expect(michaelKors.name).toBe('Michael Kors');
-    expect(michaelKors.divisionCodes).toContain('91');
-    expect(michaelKors.salesOrganizationCodes).toContain('PBE1');
-
-    // Verify details for NIKE
-    const nike = items.find((item) => item.companyCode === 'NIKE');
-    expect(nike).toBeDefined();
-    expect(nike.name).toBe('NIKE');
-    expect(nike.divisionCodes).toContain('97');
-    expect(nike.salesOrganizationCodes).toContain('PBE1');
-
-    // Verify details for Tommy Hilfiger
-    const tommyHilfiger = items.find((item) => item.companyCode === 'THEU');
-    expect(tommyHilfiger).toBeDefined();
-    expect(tommyHilfiger.name).toBe('Tommy Hilfiger');
-    expect(tommyHilfiger.divisionCodes).toContain('01');
-    expect(tommyHilfiger.divisionCodes).toContain('02');
-    expect(tommyHilfiger.divisionCodes).toContain('03');
-    expect(tommyHilfiger.divisionCodes).toContain('04');    
-    expect(tommyHilfiger.divisionCodes).toContain('05');
-    expect(tommyHilfiger.divisionCodes).toContain('07');
-    expect(tommyHilfiger.divisionCodes).toContain('09');
-    expect(tommyHilfiger.divisionCodes).toContain('10');
-    expect(tommyHilfiger.divisionCodes).toContain('11');
-    expect(tommyHilfiger.divisionCodes).toContain('18');
-    expect(tommyHilfiger.salesOrganizationCodes).toContain('THE1');
-    expect(tommyHilfiger.salesOrganizationCodes).toContain('INLC');    
-  }
-
-  // GET Divisions for a Brand API
-  function verifyResponseStructureDivisionBrand(responseBody) {
-    expect(responseBody).toHaveProperty('name');
-    expect(responseBody).toHaveProperty('code');
-    expect(responseBody).toHaveProperty('weCTimestamp');
-    expect(responseBody).toHaveProperty('modifiedOn');
-    expect(responseBody).toHaveProperty('seasons');
-    expect(Array.isArray(responseBody.seasons)).toBe(true);
-  }
-
-  function validateDateFields(responseBody) {
-    // Validate "weCTimestamp" as a Date object
-    const weCTimestamp = new Date(responseBody.weCTimestamp);
-    expect(weCTimestamp).toBeInstanceOf(Date);
-    expect(!isNaN(weCTimestamp)).toBe(true);
-
-    // Validate "modifiedOn" as a Date object
-    const modifiedOn = new Date(responseBody.modifiedOn);
-    expect(modifiedOn).toBeInstanceOf(Date);
-    expect(!isNaN(modifiedOn)).toBe(true);
-  }
-
 });
